52bca19b6e0e911f349fafb87ff1d9452636c804
Rhett Del Fierro <thinquetank@gmail.com>
2020-02-15 17:52:07 -0800

    created waveform.

--- src/command_line_query.rs
+++ src/command_line_query.rs
@@ -1,15 +1,13 @@
 use std::io;
 
-pub fn ask_for_number_of_wavetables() -> Option<u8> {
+pub fn ask_for_number_of_wavetables() -> Option<u16> {
     println!("How Stereo or Mono?");
     println!("Mono: 1");
     println!("Stereo: 2");
     let mut channels = String::new();
-    io::stdin()
-        .read_line(&mut channels)
-        .expect("No value was entered");
+    io::stdin().read_line(&mut channels);
 
-    match channels.parse() {
+    match channels.trim().parse::<u16>() {
         Ok(v) => {
             match v {
                 1 | 2 => Some(v),
@@ -21,6 +19,7 @@ pub fn ask_for_number_of_wavetables() -> Option<u8> {
         },
         Err(err) => {
             println!("Please enter a value");
+            println!("Error {}", err);
             return None;
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,28 @@
+extern crate hound;
+
+use std::f32::consts::PI;
+use std::i16;
+
 mod command_line_query;
 use command_line_query::ask_for_number_of_wavetables;
 
+fn create_waveform(number_of_channels: u16) {
+    let spec = hound::WavSpec {
+        channels: number_of_channels,
+        sample_rate: 44100,
+        bits_per_sample: 16,
+        sample_format: hound::SampleFormat::Int,
+    };
+    let mut writer = hound::WavWriter::create("waveform.wav", spec).unwrap();
+    for t in (0 .. 44100).map(|x| x as f32 / 44100.0) {
+        let sample = (t * 440.0 * 2.0 * PI).sin();
+        let amplitude = i16::MAX as f32;
+        writer.write_sample((sample * amplitude) as i16).unwrap();
+    }
+}
 fn main() {
-    let string = ask_for_number_of_wavetables();
+    match ask_for_number_of_wavetables() {
+        Some(v) => create_waveform(v),
+        None => println!("Invalid option, exiting app.")
+    }
 }
